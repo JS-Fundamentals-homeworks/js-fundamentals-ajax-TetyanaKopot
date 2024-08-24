ba99dae1defaa02dc32d80406ed9ab44f81364cc
TetyanaKopot <kopot.tetyana@gmail.com>
2024-08-24 18:40:01 +0300

    Completed tasks

--- task1.js
+++ task1.js
@@ -1,7 +1,30 @@
-// Завдання: отримання даних про користувачів 
+// Завдання: отримання даних про користувачів
 // За допомогою засобі Fetch отримати інформацію про користувачів
-// за посиланням - https://jsonplaceholder.typicode.com/users 
+// за посиланням - https://jsonplaceholder.typicode.com/users
 // Імена користувачів відобразити в ненумерованому списку ul.usersList,
 // який створений у файлі index.html
 // Запустити програму за допомогою Live Server
-// Перевірити за допомогою команди npm tests/task1.test.js 
+// Перевірити за допомогою команди npm tests/task1.test.js
+
+const userList = document.querySelector('.usersList')
+
+// ****Option 1****
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then((response) => response.json())
+  .then((users) => {
+    users.map((user) => {
+      userList.innerHTML += `<li>${user.name}</li>`
+    })
+  })
+
+// ****Option 2****
+// async function usersFetch() {
+//   const url = 'https://jsonplaceholder.typicode.com/users'
+//   const response = await fetch(url)
+//   const users = await response.json()
+//   users.map((user) => {
+//     userList.innerHTML += `<li>${user.name}</li>`
+//   })
+// }
+
+// usersFetch()
--- task2.js
+++ task2.js
@@ -5,3 +5,44 @@
 // відобразити у тезі #userCity
 // Запустити програму потрібно за допомогою Live Server
 // Перевірити правильність програми - команда node tests/task2.test.js
+
+const userNameInput = document.querySelector('#userNameInput')
+const getUserButton = document.querySelector('#getUserButton')
+const userCity = document.querySelector('#userCity')
+
+// ****Option 1****//
+// fetch('https://jsonplaceholder.typicode.com/users')
+//   .then((response) => response.json())
+//   .then((users) => {
+//     users.forEach((user) => {
+//       getUserButton.addEventListener('click', () => {
+//         if (userNameInput.value === user.name) {
+//           userCity.innerText = `${user.address.city}`
+//         }
+//       })
+//     })
+//     console.log(users)
+//   })
+
+// ****Option 2****//
+let usersData = []
+
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then((response) => response.json())
+  .then((users) => {
+    usersData = users
+  })
+  .catch((error) => {
+    console.error('Error fetching users:', error)
+  })
+
+getUserButton.addEventListener('click', () => {
+  const userName = userNameInput.value
+  const user = usersData.find((user) => user.name === userName)
+
+  if (user) {
+    userCity.innerText = user.address.city
+  } else {
+    userCity.innerText = 'User not found'
+  }
+})
